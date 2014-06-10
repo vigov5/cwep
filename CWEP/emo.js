ac647c7d5200d1e906c182a2d4b377ec422fd715
vigov5 <nguyenanhtien2210@gmail.com>
2014-06-10 15:43:21 +0700

    add clgt, some yahoo emo, public source code (0.1.8)

---
+++ CWEP/emo.js
@@ -0,0 +1,96 @@
+emo = [
+	{key: "(facepalm)", regex: /\(facepalm\)/g, src: "facepalm.gif", other_host: false},
+	{key: "(facepalm2)", regex: /\(facepalm2\)/g, src: "facepalm2.gif", other_host: false},
+	{key: "(dull)", regex: /\(dull\)/g, src: "dull.gif", other_host: false},
+	{key: "(mooning)", regex: /\(mooning\)/g, src: "mooning.gif", other_host: false},
+	{key: "(finger)", regex: /\(finger\)/g,	src: "finger.gif", other_host: false},
+	{key: "(airblade)",	regex: /\(airblade\)/g,	src: "airblade.gif", other_host: false},
+	{key: "(hehehe)", regex: /\(hehehe\)/g,	src: "hehehe.gif", other_host: false},
+	{key: "(honho)", regex: /\(honho\)/g, src: "honho.gif", other_host: false},
+	{key: "(silly)", regex: /\(silly\)/g, src: "silly.gif", other_host: false},
+	{key: "(noinham)", regex: /\(noinham\)/g, src: "noinham.png", other_host: false},
+	{key: "(uongthuoc)", regex: /\(uongthuoc\)/g, src: "uongthuoc.png", other_host: false},
+	{key: "(+1)", regex: /\(\+1\)/g, src: "plusone.png", other_host: false},
+	{key: "(-1)", regex: /\(\-1\)/g, src: "minusone.png", other_host: false},
+	{key: "(like)", regex: /\(like\)/g, src: "like.png", other_host: false},
+	{key: "(dislike)", regex: /\(dislike\)/g, src: "dislike.png", other_host: false},
+	{key: "(framgia)", regex: /\(framgia\)/g, src: "framgia.png", other_host: false},
+	{key: ":v",	regex: /\:v/g, src: "pacman.png", other_host: false},
+	{key: "(rofl)", regex: /\(rofl\)/g,	src: "rofl.gif", other_host: false},
+	{key: "(iitomo)", regex: /\(iitomo\)/g,	src: "iitomo.gif", other_host: false},
+	{key: "(imadesho)", regex: /\(imadesho\)/g,	src: "imadesho.jpeg", other_host: false},
+	{key: "(sleepy)", regex: /\(sleepy\)/g,	src: "sleepy.gif", other_host: false},
+	{key: "(venhvao)", regex: /\(venhvao\)/g,	src: "venhvao.gif", other_host: false},
+	{key: "(ifyouknowwhatimean)", regex: /\(ifyouknowwhatimean\)/g,	src: "ifyouknowwhatimean.png", other_host: false},
+	{key: "(scm)", regex: /\(scm\)/g,	src: "scm.gif", other_host: false},
+	{key: "(naruhodo)", regex: /\(naruhodo\)/g,	src: "naruhodo.gif", other_host: false},
+	{key: "(wasshoi)", regex: /\(wasshoi\)/g,	src: "wasshoi.jpeg", other_host: false},
+	{key: "(douzo)", regex: /\(douzo\)/g,	src: "douzo.png", other_host: false},
+	{key: "(otsu)", regex: /\(otsu\)/g,	src: "otsu.gif", other_host: false},
+	{key: "(canloigiaithich)", regex: /\(canloigiaithich\)/g,	src: "clgt.jpg", other_host: false},
+	{key: "(giggle)", regex: /\(giggle\)/g,	src: "giggle.gif", other_host: false},
+	{key: "(chuckle)", regex: /\(chuckle\)/g,	src: "giggle.gif", other_host: false},
+	{key: ":-&quot;", regex: /:\-&quot;/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/65.gif", other_host: true},
+	{key: "%-(", regex: /%-\(/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/107.gif", other_host: true},
+	{key: ":-??", regex: /:-\?\?/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/106.gif", other_host: true},
+	{key: ":-?", regex: /:\-\?/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/39.gif", other_host: true},
+	{key: "=P~", regex: /=P~/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/38.gif", other_host: true},
+	{key: ":@)", regex: /:@\)/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/49.gif", other_host: true},
+	{key: "3&#39;-O", regex: /3:-O/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/50.gif", other_host: true},
+	{key: "3&#39;-o", regex: /3:-o/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/50.gif", other_host: true},
+	{key: "@};-", regex: /@\};-/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/53.gif", other_host: true},
+	{key: "%%-", regex: /%%-/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/54.gif", other_host: true},
+	{key: "(~~)", regex: /\(~~\)/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/56.gif", other_host: true},
+	{key: "~O)", regex: /~O\)/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/57.gif", other_host: true},
+	{key: "8-X", regex: /8-X/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/59.gif", other_host: true},
+	{key: "8-x", regex: /8-x/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/59.gif", other_host: true},
+	{key: ":-L", regex: /:-L/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/62.gif", other_host: true},
+	{key: "$-)", regex: /\$-\)/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/64.gif", other_host: true},
+	{key: "b-(", regex: /b-\(/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/66.gif", other_host: true},
+	{key: "[-X", regex: /\[-X/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/68.gif", other_host: true},
+	{key: ":-@", regex: /:-@/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/76.gif", other_host: true},
+	{key: ":-j", regex: /:-j/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/78.gif", other_host: true},
+	{key: ":bz", regex: /:bz/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/115.gif", other_host: true},
+	{key: "[..]", regex: /\[\.\.\]/g, src: "https:\/\/s.yimg.com/lq/i/us/msg/emoticons/transformer.gif", other_host: true},
+	{key: ":-/", regex: /:-\//g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/7.gif", other_host: true},
+	{key: ":x", regex: /:x/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/8.gif", other_host: true},
+	{key: ":P", regex: /:P/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/10.gif", other_host: true},
+	{key: ":-*", regex: /:-\*/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/11.gif", other_host: true},
+	{key: ":-O", regex: /:-O/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/13.gif", other_host: true},
+	{key: "X(", regex: /X\(/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/14.gif", other_host: true},
+	{key: "B-)", regex: /B-\)/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/16.gif", other_host: true},
+	{key: ":-S", regex: /:-S/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/17.gif", other_host: true},
+	{key: "~:>", regex: /\~:&gt;/g, src: "https:\/\/s.yimg.com/lq/i/mesg/emoticons7/52.gif", other_host: true},
+];
+
+function htmlEncode(value){
+	//create a in-memory div, set it's inner text(which jQuery automatically encodes)
+	//then grab the encoded contents back out.  The div never exists on the page.
+	return $('<div/>').text(value).html();
+}
+
+function htmlDecode(value){
+	return $('<div/>').html(value).text();
+}
+
+
+$(window).ready(function(){
+	//CW.reg_cmp.push({key:/:\-"/g, rep: '<img src="https:\/\/s.yimg.com/lq/i/mesg/emoticons7/65.gif" alt="" title="" class="ui_emoticon"/>', reptxt:''});
+	CW.reg_cmp.push({key:/&gt;:D&lt;/g, rep: "<img src='https://s.yimg.com/lq/i/mesg/emoticons7/6.gif' alt='>:D<' title='>:D<' class='ui_emoticon'/>", reptxt:'>:D<'});
+	CW.prepareRegExp();
+	for (var index = 0; index < emo.length; index++) {
+		var encoded_text = htmlEncode(emo[index].key);
+		if (emo[index].other_host) {
+			img_src = emo[index].src;
+		} else {
+			img_src = 'http:\/\/cwep.thangtd.com/img/emoticons/' + emo[index].src;
+		}
+
+		CW.reg_cmp.push({
+			key: emo[index].regex,
+			rep: '<img src="' + img_src + '" title="' + encoded_text + '" alt="' +
+				encoded_text + '" class="ui_emoticon"/>',
+			reptxt: emo[index].key
+		});
+	}
+});
